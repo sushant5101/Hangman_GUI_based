0b284aa460fe46ea32bf9dddec99269b8bd4037d
sushant <115689963+sushant5101@users.noreply.github.com>
2024-12-25 07:22:19 +0545

    Update script1.1.js
    added some key words

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 const cRadiobutton = document.querySelectorAll('input[name=level]');
 // console.log("hello");
 
-cRadiobutton.forEach(radio => { radio.addEventListener('change', () => { switch(radio.value){
-    case "easy":
-        console.log("easy");
-        break;
-    
-    case "medium":
-        console.log("medium");
-        break;
+cRadiobutton.forEach(radio => {
+    radio.addEventListener('change', () => {
+        switch (radio.value) {
+            case "easy":
+                EASY_LEVEL();
+                break;
 
-    case "hard":
-        console.log("hard");
-        break;
-    
-    default:
-        console.log("failed");
-        break;
-} }); });
+            case "medium":
+                console.log("medium");
+                break;
+
+            case "hard":
+                console.log("hard");
+                break;
+
+            default:
+                console.log("failed");
+                break;
+        }
+    });
+});
+
+
+function EASY_LEVEL() {
+    console.log("easy");
+
+    var iRand_word_index = Math.floor(Math.random() * 10);
+    var iRemainiing_guesses = 8;
+    const cEasy_words = ["apple",
+        "chair",
+        "happy",
+        "house",
+        "water",
+        "light",
+        "music",
+        "pizza",
+        "river",
+        "tiger"];
+
+    console.log(cEasy_words[iRand_word_index] + " and the random number is " + iRand_word_index);
+
+    while (iRemainiing_guesses > 0) {
+        
+    }
+
+}
