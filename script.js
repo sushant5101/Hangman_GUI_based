99b0c66c5cda802bdaa525ad452f913b3b8691a9
sushant <115689963+sushant5101@users.noreply.github.com>
2024-12-26 16:31:13 +0545

    Update script1.4.js
    Updated word checking logic, changed the behavior of li element on the basis of the correct or incorrect answer given.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const cRadiobutton = document.querySelectorAll('input[name=level]');
 let used_letters = [];
 // console.log("hello");
 
+//========================referencing html ================================
+
 var submit = document.getElementById("submit");
 var user_input = document.getElementById("user_guessed_letter");
 var listbody = document.getElementById("used_letter_list");
@@ -13,7 +15,10 @@ var incorrect = 0,
     correct_count = 0,
     word_length = 0,
     temp = 0;
+let dashes = []; // Initialize an empty string
+let is_correct = false;
 
+//============================accessing all the radio buttons======================
 
 cRadiobutton.forEach(radio => {
     radio.addEventListener('change', () => {
@@ -48,39 +53,13 @@ function GAMAE_MANAGER(level) {
     submit.style.borderColor = "black"
     submit.title = "Check your guess";
     // =========================words to be used for the easy level=====================
-    const easy_words = ["apple",
-        "chair",
-        "happy",
-        "house",
-        "water",
-        "light",
-        "music",
-        "pizza",
-        "river",
-        "tiger"];
-    // ======================words to be used for medium level===================
-    const medium_words = ["bicycle",
-        "compass",
-        "library",
-        "mountain",
-        "mystery",
-        "popcorn",
-        "rainbow",
-        "scissors",
-        "volcano",
-        "whistle"];
-    // ==================words for high level difficulty===================
+    const easy_words = ["apple", "chair", "happy", "house", "water", "light", "music", "pizza", "river", "tiger"];
 
-    const hard_words = ["ostentatious",
-        "nebulous",
-        "esoteric",
-        "surreptitious",
-        "languorous",
-        "quixotic",
-        "ebullient",
-        "lugubrious",
-        "obfuscate",
-        "sesquipedalian"];
+    // ======================words to be used for medium level===================
+    const medium_words = ["bicycle", "compass", "library", "mountain", "mystery", "popcorn", "rainbow", "scissors", "volcano", "whistle"];
+
+    // ==================words for high level difficulty===================
+    const hard_words = ["ostentatious", "nebulous", "esoteric", "surreptitious", "languorous", "quixotic", "ebullient", "lugubrious", "obfuscate", "sesquipedalian"];
     // ======================================================================
     switch (level) {
 
@@ -102,7 +81,14 @@ function GAMAE_MANAGER(level) {
             word = hard_words[rand_word_index];
             break;
     }
-    word_length_label.innerText = "The word is made up of " + word_length + " words";
+
+
+    for (let i = 0; i < word_length; i++) {
+        dashes += "_ ";
+    }
+
+    word_length_label.innerText = dashes;
+    dashes = [];
 
     document.getElementById("user_guessed_letter").addEventListener("keypress", function (event) {
         if (event.key === "Enter") {
@@ -123,48 +109,78 @@ function WORD_CHECKER(word_length, word) {
 
     // console.log(word);
 
-    const guessed_letter = user_input.value.toLowerCase();
-    if (guessed_letter !== "" && !used_letters.includes(guessed_letter)) {
-        used_letters.push(guessed_letter); 
-        var new_list = document.createElement("li"); 
-        new_list.innerText = guessed_letter;
-        listbody.appendChild(new_list);
-    }
-
-    console.log(used_letters.length);
+    // console.log(used_letters.length);
 
     incorrect = 0;
-    correct_count = 0;
     for (var i = 0; i < word_length; i++) {
 
-        if (user_input.value == used_letters[i]) {
+        if (user_input.value.toLowerCase() == used_letters[i]) {
             console.log("allready entred " + user_input.value + " try another");
             break;
         }
-        else if (user_input.value == word[i]) {
+        else if (user_input.value.toLowerCase() == word[i]) {
             console.log(user_input.value + " is the correct letter");
+            console.log(dashes);
+            for (let j = 0; j < word_length; j++) {
+                if (j != i) {
+                    dashes[j] = "_ ";
+                    console.log("index " + i + " j = " + j);
+                }
+                else {
+                    dashes[i] = word[i] + " ";
+                }
+                console.log(dashes);
+                console.log("index " + i + " j = " + j);
+
+            }
+
+
+            is_correct = true;
             correct_count++;
-            used_letters = user_input.value;
         }
 
-        else if (user_input.value != word[i]) {
-            console.log("wrong");
-            used_letters.push(user_input.value);
+        else if (user_input.value.toLowerCase() != word[i]) {
+            // console.log("wrong");
             incorrect++;
         }
     }
 
+    word_length_label.innerText = dashes;
+
+
     if (incorrect == word_length) {
 
+        is_correct = false;
         remaining_guesses.innerText = remaining_guesses.innerText - 1;
         // console.log(remaining_guesses.innerText);
     }
     else if (correct_count == word_length) {
-        console.log("You found all the letters");
+        console.log("You found all the letters, the word was " + word);
         // console.log("the word was " + word);
     }
 
+    // console.log(correct_count);
+
     user_input.focus();
 
+    if (!used_letters.includes(user_input.value)) {
+        USED_ADD(user_input.value);
+    }
+
     user_input.value = '';
+}
+
+function USED_ADD(user_input) {
+    if (user_input.toLowerCase() !== "" && !used_letters.includes(user_input)) {
+        used_letters.push(user_input);
+        var new_list = document.createElement("li");
+        new_list.innerText = user_input;
+        if (is_correct) {
+            new_list.style.boxShadow = "5px 5px 5px #5CB338, 2px -5px 5px #5CB338, 5px -5px 5px #5CB338, -5px 5px 5px #5CB338";
+        } else {
+            new_list.style.boxShadow = "5px 5px 15px #FCC737, 2px -5px 15px #F26B0F, 5px -5px 15px #7E1891, -5px 5px 15px #E73879";
+        }
+
+        listbody.appendChild(new_list);
+    }
 }
