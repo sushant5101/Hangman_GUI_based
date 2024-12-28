a0ffce86e1472f84804d2a325e02cc64ac04e4c7
sushant <115689963+sushant5101@users.noreply.github.com>
2024-12-28 19:45:00 +0545

    Update data.js

--- data.js
+++ data.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
     enter.addEventListener('click', () => {
         if (username.value == "" || password.value == "") {
             alert("Fill in all the details");
-        } else {
-           fetch('https://hangman-l19x.onrender.com/data').then(response => response.json()).then(data => { data.users.push({ username: username.value, password: password.value }); fetch('https://hangman-l19x.onrender.com/data', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) }).then(response => response.text()).then(result => { console.log(result); enter.innerText = 'User added successfully'; }).catch(error => { console.error('Error saving data:', error); enter.innerText = 'Error saving data'; }); username.value = ""; password.value = ""; }).catch(error => { console.error('Error fetching JSON:', error); enter.innerText = 'Error fetching data'; });}
+        }else{
+        location.relode();
+        }
     });
-});
